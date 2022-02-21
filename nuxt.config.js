46a3f6daa1be398dbc4dba3bfa6dd76d3a595b7e
siumhossain <sium.hossain@yahoo.com>
2022-02-21 22:34:50 +0600

    ok

--- nuxt.config.js
+++ nuxt.config.js
@@ -101,9 +101,10 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://damp-wave-93524.herokuapp.com/api/',
+    baseURL: 'http://63.250.44.136/api/',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 }
+http://63.250.44.136/admin/
